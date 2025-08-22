ff27f280708f571751c51fec9b3d8ab290fd6fe5
gui-far <gui.far@outlook.com>
2025-08-22 12:48:42 -0300

    Generic Comment for Answer and Questions entities

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,40 +1,15 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface AnswerCommentProps {
-    authorId: UniqueEntityID
+export interface AnswerCommentProps extends CommentProps {
     answerId: UniqueEntityID
-    content: string
-    createdAt: Date
-    updatedAt?: Date
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
+export class AnswerComment extends Comment<AnswerCommentProps> {
 
-    get authorId() {
-        return this.props.authorId
-    }
-
-    get content() {
-        return this.props.content
-    }
-
-    get createdAt() {
-        return this.props.createdAt
-    }
-
-    get updatedAt() {
-        return this.props.updatedAt
-    }
-
-    set content(content: string) {
-        this.props.content = content
-        this.touch()
-    }
-
-    private touch() {
-        this.props.updatedAt = new Date()
+    get answerId() {
+        return this.props.answerId
     }
 
     static create(
